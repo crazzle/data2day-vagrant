a6bb17ef48c705935a758bfae32007cde06bb493
Mark Keinhörster <mark.keinhoerster@gmail.com>
2016-10-05 08:59:33 +0200

    added slides, updated Jupyter Notebook

--- scripts/bootstrap_datascience_install.sh
+++ scripts/bootstrap_datascience_install.sh
@@ -5,4 +5,4 @@ set -e
 echo "Installing Toree..."
 pip install jupyter
 pip install --pre toree
-jupyter toree install --spark_opts='--packages org.apache.spark:spark-streaming-kafka_2.10:1.6.2,org.json4s:json4s-native_2.10:3.3.0 --master=local[4]' --spark_home=/home/vagrant/spark-1.6.2-bin-hadoop2.6 --interpreters=Scala,PySpark,SparkR,SQL
+jupyter toree install --spark_opts='--packages org.apache.spark:spark-streaming-kafka_2.10:1.6.2,org.json4s:json4s-native_2.10:3.3.0,org.json4s:json4s-ext_2.10:3.3.0 --master=local[4]' --spark_home=/home/vagrant/spark-1.6.2-bin-hadoop2.6 --interpreters=Scala,PySpark,SparkR,SQL
